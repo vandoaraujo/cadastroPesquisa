90ede259c8fff783e6789179ff441f24a6e45483
Vando Ribeiro de Araujo <tecladista.vando@gmail.com>
2020-03-10 13:51:04 -0300

    first commmit

---
+++ src/main/java/com/example/cadastroPesquisa/CadastroPesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.example.cadastroPesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroPesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroPesquisaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cadastroPesquisa/CadastroPesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cadastroPesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
