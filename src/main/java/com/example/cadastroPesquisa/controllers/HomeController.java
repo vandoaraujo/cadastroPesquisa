50dcbfff46a8228d6b95e530f02ea07cabefe6ea
Vando Ribeiro de Araujo <tecladista.vando@gmail.com>
2020-03-11 14:35:12 -0300

    melhorias

--- src/main/java/com/example/cadastroPesquisa/controllers/HomeController.java
+++ src/main/java/com/example/cadastroPesquisa/controllers/HomeController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
  
-  @GetMapping("/login")
-  public String login() {
-    return "login";
-  }
+//  @GetMapping("/login")
+//  public String login() {
+//    return "login";
+//  }
   
   @GetMapping("/home")
   public String home() {
@@ -19,10 +19,5 @@ public class HomeController {
   @GetMapping("/hello")
   public String hello() {
     return "hello";
-  }
-   
-  @GetMapping("/")
-  public String index() {
-    return "index";
   }
 }
