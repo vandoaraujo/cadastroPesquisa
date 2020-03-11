ed2d39b51adbcbc9c41c91a6d51562cf17cd3b20
Vando Ribeiro de Araujo <tecladista.vando@gmail.com>
2020-03-11 14:09:02 -0300

    melhoria do css

--- src/main/java/com/cadastroPesquisa/configuration/MvcConfig.java
+++ src/main/java/com/cadastroPesquisa/configuration/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.example.securingweb;
+package com.cadastroPesquisa.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
--- src/main/java/com/cadastroPesquisa/configuration/SecurityConfig.java
+++ src/main/java/com/cadastroPesquisa/configuration/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.securingweb;
+package com.cadastroPesquisa.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,21 +10,20 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private static final String[] AUTH_LIST = {
             "/",
             "/hello",
-            "/login",
             "/home"
         };
 
         @Override
         protected void configure(HttpSecurity http) throws Exception{
             http.csrf().disable().authorizeRequests()
-                    .antMatchers(AUTH_LIST).permitAll()
-                    .anyRequest().authenticated()
-                    .and().formLogin().loginPage("/login").permitAll()
-                    .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+            .antMatchers(AUTH_LIST).permitAll()
+            .anyRequest().authenticated()
+            .and().formLogin()
+            .and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
         }
 
         @Override
@@ -35,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Override
         public void configure(WebSecurity web) throws Exception{
-            web.ignoring().antMatchers("/bootstrap/**");
+            web.ignoring().antMatchers("/static/**");
 //            web.ignoring().antMatchers("/bootstrap/**", "/style/**");
         }
 }
